7217cf5acae6e7d7c1187eede194a6edbda3659f
Abdul Ghaffar Ansari <88545317+abdulghaffaransari@users.noreply.github.com>
2024-11-23 00:08:34 +0100

    requirements.txt added and installed all packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="HarmSpeech-AI-Tracker",
+    version="0.0.1",
+    author="Abdul Ghaffar Ansari",
+    author_email="abdulghaffaransari9@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
