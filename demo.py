ba9f1c887723232a500ada031ffdbb0cef02eab8
Abdul Ghaffar Ansari <88545317+abdulghaffaransari@users.noreply.github.com>
2024-11-23 15:28:39 +0100

    gcloud_syncer.py Added

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 from hate.logger import logging
+from hate.exception import CustomException
+import sys
+from hate.configuration.gcloud_syncer import  GCloudSync
 
-logging.info("Welcome to our Project")
+# logging.info("Welcome to our Project")
+
+
+obj = GCloudSync()
+
+obj.sync_folder_from_gcloud("hate-speech-nov-2024","dataset.zip","dataset.zip")
