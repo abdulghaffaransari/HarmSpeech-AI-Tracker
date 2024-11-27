e672f761592f40b844efcfc6eb1c8c059a787f76
Abdul Ghaffar Ansari <88545317+abdulghaffaransari@users.noreply.github.com>
2024-11-27 22:05:21 +0100

    model_evaluation.py added

--- hate/pipeline/train_pipeline.py
+++ hate/pipeline/train_pipeline.py
@@ -4,19 +4,19 @@ from hate.exception import CustomException
 from hate.components.data_ingestion import DataIngestion
 from hate.components.data_transforamation import DataTransformation
 from hate.components.model_trainer import ModelTrainer
-# from hate.components.model_evaluation import ModelEvaluation
+from hate.components.model_evaluation import ModelEvaluation
 # from hate.components.model_pusher import ModelPusher
 
 from hate.entity.config_entity import (DataIngestionConfig,
                                        DataTransformationConfig,
-                                       ModelTrainerConfig)
-                                    #    ModelEvaluationConfig,
+                                       ModelTrainerConfig,
+                                       ModelEvaluationConfig)
                                     #    ModelPusherConfig)
 
 from hate.entity.artifact_entity import (DataIngestionArtifacts,
                                          DataTransformationArtifacts,
-                                         ModelTrainerArtifacts)
-                                        #  ModelEvaluationArtifacts,
+                                         ModelTrainerArtifacts,
+                                         ModelEvaluationArtifacts)
                                         #  ModelPusherArtifacts)
 
 
@@ -25,7 +25,7 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
-        # self.model_evaluation_config =ModelEvaluationConfig()
+        self.model_evaluation_config =ModelEvaluationConfig()
         # self.model_pusher_config = ModelPusherConfig()
 
 
@@ -88,19 +88,19 @@ class TrainPipeline:
         
 
     
-    # def start_model_evaluation(self, model_trainer_artifacts: ModelTrainerArtifacts, data_transformation_artifacts: DataTransformationArtifacts) -> ModelEvaluationArtifacts:
-    #     logging.info("Entered the start_model_evaluation method of TrainPipeline class")
-    #     try:
-    #         model_evaluation = ModelEvaluation(data_transformation_artifacts = data_transformation_artifacts,
-    #                                             model_evaluation_config=self.model_evaluation_config,
-    #                                             model_trainer_artifacts=model_trainer_artifacts)
+    def start_model_evaluation(self, model_trainer_artifacts: ModelTrainerArtifacts, data_transformation_artifacts: DataTransformationArtifacts) -> ModelEvaluationArtifacts:
+        logging.info("Entered the start_model_evaluation method of TrainPipeline class")
+        try:
+            model_evaluation = ModelEvaluation(data_transformation_artifacts = data_transformation_artifacts,
+                                                model_evaluation_config=self.model_evaluation_config,
+                                                model_trainer_artifacts=model_trainer_artifacts)
 
-    #         model_evaluation_artifacts = model_evaluation.initiate_model_evaluation()
-    #         logging.info("Exited the start_model_evaluation method of TrainPipeline class")
-    #         return model_evaluation_artifacts
+            model_evaluation_artifacts = model_evaluation.initiate_model_evaluation()
+            logging.info("Exited the start_model_evaluation method of TrainPipeline class")
+            return model_evaluation_artifacts
 
-    #     except Exception as e:
-    #         raise CustomException(e, sys) from e
+        except Exception as e:
+            raise CustomException(e, sys) from e
         
     
 
@@ -139,12 +139,12 @@ class TrainPipeline:
                 data_transformation_artifacts=data_transformation_artifacts
             )
 
-            # model_evaluation_artifacts = self.start_model_evaluation(model_trainer_artifacts=model_trainer_artifacts,
-            #                                                         data_transformation_artifacts=data_transformation_artifacts
-            # ) 
+            model_evaluation_artifacts = self.start_model_evaluation(model_trainer_artifacts=model_trainer_artifacts,
+                                                                    data_transformation_artifacts=data_transformation_artifacts
+            ) 
 
-            # if not model_evaluation_artifacts.is_model_accepted:
-            #     raise Exception("Trained model is not better than the best model")
+            if not model_evaluation_artifacts.is_model_accepted:
+                raise Exception("Trained model is not better than the best model")
             
             # model_pusher_artifacts = self.start_model_pusher()
 
